74ce4eef410be337d473198d50e683d9d79c32a3
Søren Hansen <sorenviggo@hotmail.com>
2019-10-16 13:32:59 +0000

    Modified UI5 view and controller to call PDF utility scripts

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,11 +1,23 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"dk/sorenviggo/ui5/jspdf/jsPDF_Create/utils/PdfCreator"
+], function (Controller, PdfCreator) {
 	"use strict";
 
 	return Controller.extend("dk.sorenviggo.ui5.jspdf.jsPDF_Create.controller.Main", {
 		onInit: function () {
 
+		},
+		
+		onOpenPDF: function(oEvent){
+			PdfCreator.createPdfDoc();
+			PdfCreator.openFile();
+		},
+
+		onSavePDF: function(oEvent){
+			PdfCreator.createPdfDoc();
+			PdfCreator.saveFile("Test.pdf");
 		}
+
 	});
 });
